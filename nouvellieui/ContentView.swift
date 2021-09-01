90b62e7fc598cf04d0fc72bbc362b9e52d6600a3
Rocuant Roberto <atrysrocuant@MacBook-Pro-de-Rocuant.local>
2021-08-31 20:41:53 -0400

    first commit

---
+++ nouvellieui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nouvellieui
+//
+//  Created by Rocuant Roberto on 31-08-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nouvellieui/nouvellieuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  nouvellieuiApp.swift
+//  nouvellieui
+//
+//  Created by Rocuant Roberto on 31-08-21.
+//
+
+import SwiftUI
+
+@main
+struct nouvellieuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
